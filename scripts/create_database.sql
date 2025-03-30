7be0d5120f3ba96a3b6e79665476ac2c70ef255e
Leonardo Mundim <mundim.lt@gmail.com>
2025-03-30 18:31:25 -0300

    Update create_database.sql

--- scripts/create_database.sql
+++ scripts/create_database.sql
@@ -1,1 +1,28 @@
+USE master
 
+GO
+
+-- Verifica se existe o banco de dados, para excluí-lo e criar um novo
+IF EXISTS (SELECT name FROM sys.databases WHERE name = 'ComercialDB')
+BEGIN
+    ALTER DATABASE ComercialDB SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+    DROP DATABASE ComercialDB;
+END;
+GO
+
+-- Criação do banco de dados
+CREATE DATABASE ComercialDB;
+GO
+
+USE ComercialDB;
+GO
+
+-- Criação dos schemas
+CREATE SCHEMA bronze;
+GO
+
+CREATE SCHEMA silver;
+GO
+
+CREATE SCHEMA gold;
+GO
