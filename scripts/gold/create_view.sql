d4f5ea442541c7888cc7c87dae7da560775c237b
Leonardo Mundim <mundim.lt@gmail.com>
2025-03-31 13:56:45 -0300

    Update create_view.sql

--- scripts/gold/create_view.sql
+++ scripts/gold/create_view.sql
@@ -15,10 +15,10 @@ CREATE VIEW gold.dim_clientes AS
 
 SELECT 
 	DISTINCT(vendas_cliente_chave) AS id_cliente,
-	vendas_cliente AS nome_cliente,
-	vendas_segmento AS segmento,
-	vendas_regiao AS regiao,
-	vendas_estado AS estado
+	vendas_cliente 		       AS nome_cliente,
+	vendas_segmento 	       AS segmento,
+	vendas_regiao 	               AS regiao,
+	vendas_estado  	 	       AS estado
 FROM silver.vendas;
 
 GO
@@ -34,9 +34,9 @@ CREATE VIEW gold.dim_produtos AS
 
 SELECT
 	DISTINCT(vendas_id_produto) AS id_produto,
-	vendas_produto AS produto,
-	vendas_preco AS preco_unitario,
-	vendas_custo AS custo_unitario
+	vendas_produto 	   	    AS produto,
+	vendas_preco 		    AS preco_unitario,
+	vendas_custo 		    AS custo_unitario
 FROM silver.vendas
 
 GO
@@ -51,31 +51,19 @@ GO
 CREATE VIEW gold.dim_vendas AS
 
 SELECT
-	vendas_chave_transacao  AS id_venda,
-	vendas_data_transacao AS data_venda,
-	vendas_cliente AS cliente,
-	vendas_cliente_chave AS id_cliente,
-	vendas_estado AS estado,
-	vendas_id_produto AS id_produto,
-	vendas_produto AS produto,
-	vendas_preco AS preco_unitario,
-	vendas_quantidade AS quantidade,
-	vendas_total AS valor_total,
-	vendas_desconto AS desconto_percentual,
-	CAST(
-		CAST(
-			SUBSTRING(vendas_desconto, 1, LEN(vendas_desconto) - 1) 
-        AS DECIMAL(10,2)
-		) / 100 * vendas_total 
-    AS DECIMAL(10,2)
-	) AS desconto_valor,
-	vendas_total - 	CAST(
-						CAST(
-							SUBSTRING(vendas_desconto, 1, LEN(vendas_desconto) - 1) 
-							AS DECIMAL(10,2)
-						) / 100 * vendas_total 
-						AS DECIMAL(10,2)
-					) AS total_com_desconto
+	vendas_chave_transacao  	AS id_venda,
+	vendas_data_transacao 	   	AS data_venda,
+	vendas_cliente 		        AS cliente,
+	vendas_cliente_chave 	        AS id_cliente,
+	vendas_estado 			AS estado,
+	vendas_id_produto 	        AS id_produto,
+	vendas_produto 			AS produto,
+	vendas_preco 			AS preco_unitario,
+	vendas_quantidade 		AS quantidade,
+	vendas_total 		        AS valor_total,
+	vendas_desconto 		AS desconto_percentual,
+	CAST(CAST(SUBSTRING(vendas_desconto, 1, LEN(vendas_desconto) - 1) AS DECIMAL(10,2)) / 100 * vendas_total AS DECIMAL(10,2)) AS desconto_valor,
+	vendas_total - CAST(CAST(SUBSTRING(vendas_desconto, 1, LEN(vendas_desconto) - 1) AS DECIMAL(10,2)) / 100 * vendas_total AS DECIMAL(10,2)) AS desconto_valor
 FROM silver.vendas
 
 
