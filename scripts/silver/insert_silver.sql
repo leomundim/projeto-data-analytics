e0fdd52cd4af087d891cc4dba9975b9bb03565a8
Leonardo Mundim <mundim.lt@gmail.com>
2025-04-07 10:44:28 -0300

    Update insert_silver.sql

--- scripts/silver/insert_silver.sql
+++ scripts/silver/insert_silver.sql
@@ -9,18 +9,13 @@ TRUNCATE TABLE silver.ads;
 INSERT INTO silver.ads(
 	ads_data,
 	ads_midia,
-	ads_valor,
-	ads_mes,
-	ads_ano,
-	ads_campanha
+	ads_valor
+
 )
 SELECT
-	data_ads,
+	[data],
 	midia,
-	valor,
-	mês,
-	ano,
-	campanha
+	valor
 FROM bronze.ads
 
 
@@ -28,32 +23,31 @@ FROM bronze.ads
 
 TRUNCATE TABLE silver.leads;
 INSERT INTO silver.leads(
-	lead_ID,
-	lead_fonte,
-	lead_status,
-	lead_scoring,
-	lead_engajamento,
-	lead_datacriacao,
-	lead_dataconversao
+	lead_ID,              
+	lead_data,			 
+	lead_fonte,           
+	lead_scoring,        
+	lead_engajamento,     
+	lead_whats,           
+	lead_amostras,		
+	lead_proposta,		 
+	lead_compra			 
 )
 SELECT 
 	ID_lead,
+	Data_conversão,
 	fonte_do_lead,
-	status_atual,
 	CASE 
 		WHEN scoring IS NULL THEN 0
 		ELSE scoring 
 	END AS scoring,
 	engagement_score,
-	data_de_criação,
-	CASE
-		WHEN data_de_conversão IS NULL then data_de_criação
-		ELSE data_de_conversão
-	END as data_de_conversão_ajustada
-	-- Quando a data de conversão estiver vazia, será considerado a data de criação como data de conversão, 
-	-- visto que na maior parte dos casos, os leads são convertidos no mesmo dia.
+	whatsapp,
+	amostras,
+	proposta,
+	compra
 FROM bronze.leads
-WHERE data_de_criação IS NOT NULL 
+ 
 
 
 
@@ -63,39 +57,46 @@ TRUNCATE TABLE silver.vendas;
 INSERT INTO silver.vendas (
 	vendas_index,
 	vendas_chave_transacao,
+	vendas_source,
 	vendas_data_transacao,
 	vendas_estado,
 	vendas_regiao,
 	vendas_cliente,
-	vendas_segmento,
 	vendas_cliente_chave,
+	vendas_segmento,	
 	vendas_id_produto,
 	vendas_produto,
 	vendas_preco,
 	vendas_custo,
 	vendas_desconto,
 	vendas_quantidade,
-	vendas_total
+	vendas_total1,
+	vendas_total2
 )
 SELECT
 	[index],
 	venda_chave,
+	CASE
+		WHEN fonte = 'mkt' THEN 'Marketing' 
+		ELSE 'Base Clientes' -- Quando houver dados nulos, será considerada a informação 'Base Clientes'
+	END AS origem_cliente,
 	data_venda,
 	estado,
 	regiao,
-	LTRIM(SUBSTRING(cliente, CHARINDEX(' ', cliente) + 1, LEN(cliente))) AS cliente, -- Ajuste de texto para manter somente o nome
+	Cliente,
 	segmento,
-	cliente_chave,
+	CONCAT(cliente_chave, '-', estado) AS cliente_chave, -- Juntando o código do cliente com o Estado para formar um código único
 	id_produto,
 	produto,
 	preco/100 AS preco_unitario, -- Transformando o preço para valor unitário
-	custo/100 as custo_unitario, -- Transoformando o custo para valor unitário
+	custo/100 as custo_unitario, -- Transformando o custo para valor unitário
 	desconto,
 	quantidade,
 	CASE 
 		WHEN preco * quantidade / 100 = total_venda/100 THEN total_venda / 100
 		ELSE preco * quantidade / 100
-	END AS total_venda2 -- O valor total da venda precisa ser o preço * quantidade, esse será o valor considerado correto.
+	END AS total_venda1, -- O valor total da venda precisa ser o preço * quantidade, esse será o valor considerado correto.
+	total
  FROM bronze.vendas
 	
 END TRY
