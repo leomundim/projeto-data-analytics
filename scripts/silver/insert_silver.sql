fc6c68fef1ab639098904d73858f607330eb3e01
Leonardo Mundim <mundim.lt@gmail.com>
2025-04-15 17:44:32 -0300

    Update insert_silver.sql

--- scripts/silver/insert_silver.sql
+++ scripts/silver/insert_silver.sql
@@ -1,59 +1,128 @@
---Verifica se existe a tabela, faz a exclusão e cria uma nova
+-- Carregamento dos dados das tabelas ads, leads e vendas da camada bronze, para a camada silver.
+-- Limpeza e normalização de dados.
 
+BEGIN TRY
 
-IF OBJECT_ID('silver.ads', 'U') IS NOT NULL
-    DROP TABLE silver.ads;
-GO
-
-CREATE TABLE silver.ads (
-   	ads_data            DATE NULL,
-    	ads_midia           NVARCHAR(50) NULL,
-    	ads_valor           DECIMAL(10,2) NULL,
-
-   
-);
-GO
-
-IF OBJECT_ID('silver.leads', 'U') IS NOT NULL
-    DROP TABLE silver.leads;
-GO
-
-CREATE TABLE silver.leads (
-	lead_ID              INT NULL,
-	lead_data	     DATE NULL,
-	lead_fonte           NVARCHAR(50) NULL,
-	lead_scoring         INT NULL,
-	lead_engajamento     INT NULL,
-	lead_whats           NVARCHAR(50) NULL,
-	lead_amostras	     NVARCHAR(50) NULL,
-	lead_proposta	     NVARCHAR(50) NULL,
-	lead_compra	     NVARCHAR(50) NULL,
-
-   
-);
-GO
-
-IF OBJECT_ID('silver.vendas', 'U') IS NOT NULL
-    DROP TABLE silver.vendas;
-GO
-
-CREATE TABLE silver.vendas (
-        vendas_index             INT NULL,
-	vendas_chave_transacao   NVARCHAR(50) NULL,
-	vendas_source		 NVARCHAR(50) NULL,
-	vendas_data_transacao    DATE NULL,
-	vendas_estado            NVARCHAR(50) NULL,
-	vendas_regiao            NVARCHAR(50) NULL,
-	vendas_cliente           NVARCHAR(50) NULL,
-	vendas_cliente_chave     NVARCHAR(100)NULL,
-	vendas_segmento          NVARCHAR(50) NULL,	
-	vendas_id_produto        NVARCHAR(50) NULL,
-	vendas_produto           NVARCHAR(50) NULL,
-	vendas_preco             DECIMAL(10,2) NULL,
-	vendas_custo             DECIMAL(10,2) NULL,
-	vendas_desconto          NVARCHAR(50) NULL,
-	vendas_quantidade        INT NULL,
-	vendas_total1		 DECIMAL(10,2) NULL,
-	vendas_total2            DECIMAL(10,2) NULL,
+  -- Carregamentos dos dados da tabela 'ads' para a camada silver
+  
+TRUNCATE TABLE silver.ads;
+INSERT INTO silver.ads(
+	ads_data,
+	ads_midia,
+	ads_valor
 
 )
+SELECT
+	[data],
+	midia,
+	valor
+FROM bronze.ads
+
+
+-- Carregamentos dos dados da tabela 'leads' para a camada silver
+
+TRUNCATE TABLE silver.leads;
+INSERT INTO silver.leads(
+	lead_ID,              
+	lead_data,			 
+	lead_fonte,           
+	lead_scoring,        
+	lead_engajamento,     
+	lead_whats,           
+	lead_amostras,		
+	lead_proposta,		 
+	lead_compra			 
+)
+SELECT 
+	ID_lead,
+	Data_conversão,
+	fonte_do_lead,
+	CASE 
+		WHEN scoring IS NULL THEN 0
+		ELSE scoring 
+	END AS scoring,
+	engagement_score,
+	whatsapp,
+	amostras,
+	proposta,
+	compra
+FROM bronze.leads
+ 
+
+
+
+-- Carregamentos dos dados da tabela 'vendas' para a camada silver
+
+TRUNCATE TABLE silver.vendas;
+INSERT INTO silver.vendas (
+	vendas_index,
+	vendas_chave_transacao,
+	vendas_source,
+	vendas_data_transacao,
+	vendas_estado,
+	vendas_regiao,
+	vendas_cliente,
+	vendas_cliente_chave,
+	vendas_segmento,	
+	vendas_id_produto,
+	vendas_produto,
+	vendas_preco,
+	vendas_custo,
+	vendas_desconto,
+	vendas_quantidade,
+	vendas_total1,
+	vendas_total2
+)
+SELECT
+	[index],
+	venda_chave,
+	CASE
+		WHEN fonte = 'mkt' THEN 'Marketing' 
+		ELSE 'Base Clientes' -- Quando houver dados nulos, será considerada a informação 'Base Clientes'
+	END AS origem_cliente,
+	data_venda,
+	estado,
+	regiao,
+	Cliente,
+	CONCAT(cliente_chave, '-', estado) AS cliente_chave, -- Juntando o código do cliente com o Estado para formar um código único
+	CASE 
+		WHEN Cliente LIKE '%indústria%' OR Cliente LIKE '%fábrica%' THEN 'Indústria'
+		WHEN CLiente LIKE '%restaurante%' OR Cliente LIKE '%gastro%' OR Cliente LIKE '%forno%' THEN 'Restaurante'
+		WHEN Cliente LIKE '%padaria%' OR Cliente LIKE '%pão%' THEN 'Padaria Artesanal'
+		WHEN Cliente LIKE '%torta%' OR Cliente LIKE '%trufa%' OR Cliente LIKE '%confeitaria%' THEN 'Torteria'
+		WHEN Cliente LIKE '%café%' OR Cliente LIKE '%bistrô%' THEN 'Café & Bistrô'
+		ELSE 'Café & Bistrô'
+	END AS segmento, -- Ajustando os segmentos baseados no nome do cliente, uma vez que na camada bronze os dados vieram misturados.
+	id_produto,
+	produto,
+	preco/100 AS preco_unitario, -- Transformando o preço para valor unitário
+	custo/100 as custo_unitario, -- Transformando o custo para valor unitário
+	desconto,
+	quantidade,
+	CASE 
+		WHEN preco * quantidade / 100 = total_venda/100 THEN total_venda / 100
+		ELSE preco * quantidade / 100
+	END AS total_venda1, -- O valor total da venda precisa ser o preço * quantidade, esse será o valor considerado correto.
+	total
+ FROM bronze.vendas
+	
+END TRY
+	BEGIN CATCH
+	 	PRINT '=========================================='
+		PRINT 'Erro durante o load para camada silver'
+		PRINT 'Erro' + ERROR_MESSAGE();
+		PRINT 'Erro' + CAST (ERROR_NUMBER() AS NVARCHAR);
+		PRINT 'Erro' + CAST (ERROR_STATE() AS NVARCHAR);
+		PRINT '=========================================='
+	END CATCH	
+
+
+
+
+
+
+
+
+
+
+	
